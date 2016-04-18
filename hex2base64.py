1b3bd78d2dd4cb9ddb44a0d1558a34bd3a341007
Yifan-Tian <yifan.tian@my.erau.edu>
2016-04-18 16:12:53 -0400

    hex2base64.py

--- hex2base64.py
+++ hex2base64.py
@@ -3,9 +3,14 @@ s = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7
 #convert to:
 #"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
-import argparse
+import sys
 
 def hex2base64(string):
     return string.decode('hex').encode('base64')
 
-print hex2base64(s)
+def main():
+    print "##################################"
+    print hex2base64(sys.argv[1])
+    print "##################################"
+
+main()
